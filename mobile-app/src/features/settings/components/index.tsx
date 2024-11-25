d2c7892603834a5460696b2275a5cc76cc64986c
FSaal <f.saalfrank@gmail.com>
2024-11-25 17:37:32 +0100

    Use one index file per feature

--- mobile-app/src/features/settings/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { CalibrationControls } from './calibration_controls';
-export { ConnectionCard } from './connection_card';
-export { ConnectionControls } from './connection_controls';
-export { CalibrationModal } from './modals/calibration_modal';
---
+++ mobile-app/src/features/settings/index.tsx
@@ -0,0 +1,5 @@
+export { CalibrationControls } from './components/calibration_controls';
+export { ConnectionCard } from './components/connection_card';
+export { ConnectionControls } from './components/connection_controls';
+export { CalibrationModal } from './components/modals/calibration_modal';
+export { useDeviceSettings } from './hooks/use_device_settings';
